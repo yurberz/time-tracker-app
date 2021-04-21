72d83eab71bfe1e712c5047515dd9c4eece89235
Yurii Berezynets <71183793+yurberz@users.noreply.github.com>
2021-04-21 15:55:17 +0300

    Delete src/redux directory

--- src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import timeTrackerReducer from "./timeTrackerReducer";
-
-const rootReducer = combineReducers({
-  timeTracker: timeTrackerReducer,
-});
-
-export default rootReducer;
